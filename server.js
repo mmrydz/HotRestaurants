351c314b255c28997c49817cafecb3fa1fbf6dae
Bryan Villatoro <bvillatoro@ymail.com>
2019-04-16 13:47:59 -0400

    server functionallity

---
+++ server.js
@@ -0,0 +1,111 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let tables = [
+
+
+    {
+        routeName: "tableroute",
+        name: "name",
+        table: 51,
+        queuePosition: 1,
+    }
+]
+
+let waitlist =
+    [
+        {
+            routeName: "waitroute",
+            name: "name",
+            table: 51,
+            queuePosition: 1,
+        }
+    ]
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+
+app.get("/reservation", function (req, res) {
+    res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+app.get("/api/tables", function (req, res) {
+    return res.json(tables);
+});
+
+app.get("/api/waitlist", function (req, res) {
+    return res.json(waitlist);
+});
+
+app.get("/api/tables/:table", function (req, res) {
+    var chosen = req.params.table;
+
+    console.log(chosen);
+
+    for (var i = 0; i < tables.length; i++) {
+        if (chosen === tables[i].routeName) {
+            return res.json(tables[i]);
+        }
+    }
+})
+app.get("/api/waitlist/:waitlist", function (req, res) {
+    var chosen = req.params.waitlist;
+
+    console.log(chosen);
+
+    for (var i = 0; i < waitlist.length; i++) {
+        if (chosen === waitlist[i].routeName) {
+            return res.json(waitlist[i]);
+        }
+    }
+})
+
+app.post("/api/tables", function (req, res) {
+    var newtable = req.body;
+
+    newtable.routeName = newtable.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newtable);
+
+    tables.push(newtable);
+
+    res.json(newtable);
+});
+
+
+app.post("/api/waitlist", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newwait = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newwait
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newwait.routeName = newwait.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newwait);
+
+    waitlist.push(newwait);
+
+    res.json(newwait);
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
